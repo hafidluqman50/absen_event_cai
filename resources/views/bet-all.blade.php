911cedc0a3e8694bb695effeab454aeb42d679e1
khoirulli nurul fatimah <khoirullinurul.41@gmail.com>
2018-07-03 00:59:02 +0800

    cekcek

--- resources/views/bet-all.blade.php
+++ resources/views/bet-all.blade.php
@@ -1,98 +1,209 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bet Peserta | Ansensi Online</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link href="{{asset('frontend/custom/peserta.css')}}" rel="stylesheet">
-    <link rel="stylesheet" src="{{asset('frontend/custom/print.css')}}" type="text/css" media="print" />
-    <style type="text/css" media="print">
-        @media print{
-            *{-webkit-print-color-adjust:exact;print-color-adjust:exact}
-            @page {
-                /* page-break-after: always; */
-                size:F4;
-            }
+  <meta charset="utf-8">
+  <title>Bet Peserta</title>
+ <!-- Normalize or reset CSS with your favorite library -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
 
-            @page{
-                margin:1cm;
-                margin-top:3cm;
-                margin-bottom:1cm;
-                page-break-after: auto;
-                /* margin-top: -3.2cm;
-                margin-left:0.5cm;   
-                margin-right:0.5cm;   
-                margin-bottom:-3.2cm;  */
-                /* Top margin on first page 10cm */
-            }
-            
+<!-- Load paper.css for happy printing -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.3.0/paper.css">
+<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<link href="{{asset('frontend/custom/peserta.css')}}" rel="stylesheet">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Libre+Barcode+128+Text|Lobster|Montserrat|Source+Sans+Pro:700');
+    @page { size: legal }
+    
+    @media print { 
 
-            /* *{
-                -webkit-print-color-adjust: exact!important;
-            } */
+    }
+    .label{
+        /* Avery 5160 labels -- CSS and HTML by MM at Boulder Information Services */
+        width: 446px; /* plus .6 inches from padding */
+        /* height: .670in; */
+        height: 16cm; /* plus .125 inches from padding */
+        padding: .10in 0in 0;
+        margin-right: .125in; /* the gutter */
+        margin-bottom: .125in; /* the gutter */
+        float: left;
+        display: block;
+        text-align: center;
+        overflow: hidden;
+        outline: 1px solid grey; 
         }
-    </style>
+      .page-break {
+        clear: both;
+        display:block;
+        page-break-after:always;
+        }
+      .text-nama {
+        font-family: 'Share Tech Mono';
+        font-size: 8pt;
+        width:100%;
+        text-align: center;
+        line-height: 2pt;
+      }
+      .bar{
+          font-family: 'Libre Barcode 128 Text', cursive;
+          font-size:27pt;
+      }
+  </style>
 </head>
-<body>
-    <div class="container-fluid">
-        <div class="row text-align">
-            {{-- <div class="col-md-3">
-                <div class="bgpeserta" style="width:302.3622047244088px; height:377.952755905511px; margin:0.5rem">
-                    <div class="col-md-12 row" style="top:1rem; left:0.7rem">
-                        <div class="col-md-3 text-center">
-                            <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
-                        </div>
-                        <div class="col-md-9 text-center" style="left:0.8rem">
-                            <p class="judul">PERMATA XXXIX</p>
-                            <p class="ket">CINTA ALAM INDONESIA</p>
-                            <p class="thn">2018</p>
-                        </div>
-                    </div>
-                    <div class="col-md-12 text-center" style="left:1rem; top:1rem">
-                        <div class="kotak text-center">
-                            <label class="status">Peserta</label>
-                        </div>
-                    </div>
-                    <div class="col-md-12 text-center" style="top:2rem">
-                        <label class="inp">Khoirulli Nurul Fatimah</label>
-                        <label class="inp-kel">Sambutan</label>
-                        <label class="inp-bar">165634689567</label>
-                    </div>
-                </div>
-            </div> --}}
-            @foreach($get as $key => $value)
-            <div class="{{ $value->ket == 'panitia' ? 'bgpanitia' : ($value->ket == 'peserta' ? 'bgpeserta' : '') }}" style="width:13cm; height:17cm; margin-right:2rem;margin-bottom:8rem">
-                <div class="col-md-12 row" style="top:1rem; left:0.7rem">
-                    <div class="col-md-3 text-center">
-                        <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
-                    </div>
-                    <div class="col-md-9 text-center" style="left:2.8rem; margin-top:0.5rem">
-                        <p class="judul">PERMATA XXXIX</p>
-                        <p class="ket">{{ $value->nama_kegiatan }}</p>
-                        <p class="thn">{{ year($value->tanggal_kegiatan) }}</p>
-                    </div>
-                </div>
-                <div class="col-md-12 text-center" style="left:5.6rem; top:1.9rem">
-                    <div class="kotak text-center">
-                        <label class="status">{{ $value->ket == 'panitia' ? 'Panitia' : ($value->ket == 'peserta' ? 'Peserta' : '') }}</label>
-                    </div>
-                </div>
-                <br>
-                <div class="col-md-12 text-center" style="top:2rem">
-                    <label class="inp   ">{{ $value->nama_anggota }}</label>
-                    <label class="inp-kel">{{ $value->nama_kelompok }}</label>
-                    {{--<label class="inp-bar"><img src="data:image/png;base64,{{$barcode->barcode($value->code_barcode)}}" alt="barcode"/>{{ $value->code_barcode }}</label>--}}
-                    <label class="inp-bar"><img src="data:image/png;base64,{{$barcode->barcode($value->code_barcode)}}"> {{--{{ $value->code_barcode }} --}}</label>
-                </div>
+<body class="legal">
+<section class="sheet padding-10mm">
+    {{-- <div class="label">
+        <label class="bar"><img src="data:image/png;base64,{{ $barcode->code($data->code_barcode) }}" alt=""></label><br>
+        <span class="text-nama">{{ $data->nama_anggota }}</span>
+    </div> --}}
+    {{-- <div class="label">
+        <label class="bar"></label><br>
+        <span class="text-nama">hm</span>
+    </div> --}}
+
+    <div class="label bgpeserta" style="">
+        <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+            <div class="col-md-3 text-center">
+                <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
             </div>
-            @if($key+1%4 == 0)
-            <div class="setpaper"></div>
-            @endif
-            @endforeach
+            <div class="col-md-9 text-center" style="left:3rem; margin-top:0.5rem">
+                <p class="judul">PERMATA XXXIX</p>
+                <p class="ket">CINTA ALAM INDONESIA</p>
+                <p class="thn">2018</p>
+            </div>
+        </div>
+        <div class="col-md-12 text-center" style="left:5.5rem; top:1.9rem">
+            <div class="kotak text-center">
+                <label class="status">Peserta</label>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12 text-center" style="top:2rem">
+            <label class="inp   ">Khoirulli Nurul Fatimah</label>
+            <label class="inp-kel">Sambutan</label>
+            <label class="inp-bar">2941533150001</label>
         </div>
     </div>
+
+    <div class="label bgpeserta" style="">
+        <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+            <div class="col-md-3 text-center">
+                <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+            </div>
+            <div class="col-md-9 text-center" style="left:3rem; margin-top:0.5rem">
+                <p class="judul">PERMATA XXXIX</p>
+                <p class="ket">CINTA ALAM INDONESIA</p>
+                <p class="thn">2018</p>
+            </div>
+        </div>
+        <div class="col-md-12 text-center" style="left:5.5rem; top:1.9rem">
+            <div class="kotak text-center">
+                <label class="status">Peserta</label>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12 text-center" style="top:2rem">
+            <label class="inp   ">Khoirulli Nurul Fatimah</label>
+            <label class="inp-kel">Sambutan</label>
+            <label class="inp-bar">2941533150001</label>
+        </div>
+    </div>
+
+    <div class="label bgpeserta" style="">
+        <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+            <div class="col-md-3 text-center">
+                <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+            </div>
+            <div class="col-md-9 text-center" style="left:3rem; margin-top:0.5rem">
+                <p class="judul">PERMATA XXXIX</p>
+                <p class="ket">CINTA ALAM INDONESIA</p>
+                <p class="thn">2018</p>
+            </div>
+        </div>
+        <div class="col-md-12 text-center" style="left:5.5rem; top:1.9rem">
+            <div class="kotak text-center">
+                <label class="status">Peserta</label>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12 text-center" style="top:2rem">
+            <label class="inp   ">Khoirulli Nurul Fatimah</label>
+            <label class="inp-kel">Sambutan</label>
+            <label class="inp-bar">2941533150001</label>
+        </div>
+    </div>
+
+    <div class="label bgpeserta" style="">
+        <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+            <div class="col-md-3 text-center">
+                <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+            </div>
+            <div class="col-md-9 text-center" style="left:3rem; margin-top:0.5rem">
+                <p class="judul">PERMATA XXXIX</p>
+                <p class="ket">CINTA ALAM INDONESIA</p>
+                <p class="thn">2018</p>
+            </div>
+        </div>
+        <div class="col-md-12 text-center" style="left:5.5rem; top:1.9rem">
+            <div class="kotak text-center">
+                <label class="status">Peserta</label>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12 text-center" style="top:2rem">
+            <label class="inp   ">Khoirulli Nurul Fatimah</label>
+            <label class="inp-kel">Sambutan</label>
+            <label class="inp-bar">2941533150001</label>
+        </div>
+    </div>
+
+    <div class="label bgpeserta" style="">
+        <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+            <div class="col-md-3 text-center">
+                <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+            </div>
+            <div class="col-md-9 text-center" style="left:3rem; margin-top:0.5rem">
+                <p class="judul">PERMATA XXXIX</p>
+                <p class="ket">CINTA ALAM INDONESIA</p>
+                <p class="thn">2018</p>
+            </div>
+        </div>
+        <div class="col-md-12 text-center" style="left:5.5rem; top:1.9rem">
+            <div class="kotak text-center">
+                <label class="status">Peserta</label>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12 text-center" style="top:2rem">
+            <label class="inp   ">Khoirulli Nurul Fatimah</label>
+            <label class="inp-kel">Sambutan</label>
+            <label class="inp-bar">2941533150001</label>
+        </div>
+    </div>
+
+    <div class="label bgpeserta" style="">
+        <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+            <div class="col-md-3 text-center">
+                <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+            </div>
+            <div class="col-md-9 text-center" style="left:3rem; margin-top:0.5rem">
+                <p class="judul">PERMATA XXXIX</p>
+                <p class="ket">CINTA ALAM INDONESIA</p>
+                <p class="thn">2018</p>
+            </div>
+        </div>
+        <div class="col-md-12 text-center" style="left:5.5rem; top:1.9rem">
+            <div class="kotak text-center">
+                <label class="status">Peserta</label>
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12 text-center" style="top:2rem">
+            <label class="inp   ">Khoirulli Nurul Fatimah</label>
+            <label class="inp-kel">Sambutan</label>
+            <label class="inp-bar">2941533150001</label>
+        </div>
+    </div>
+</section>
 </body>
 </html>
---
+++ resources/views/bet-all1.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bet Peserta | Ansensi Online</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="{{asset('frontend/custom/peserta.css')}}" rel="stylesheet">
+    <link rel="stylesheet" src="{{asset('frontend/custom/print.css')}}" type="text/css" media="print" />
+    <style type="text/css" media="print">
+        @media print{
+            *{-webkit-print-color-adjust:exact;print-color-adjust:exact}
+            @page {
+                /* page-break-after: always; */
+                size:F4;
+            }
+
+            @page{
+                margin:1cm;
+                margin-top:3cm;
+                margin-bottom:1cm;
+                page-break-after: auto;
+                /* margin-top: -3.2cm;
+                margin-left:0.5cm;   
+                margin-right:0.5cm;   
+                margin-bottom:-3.2cm;  */
+                /* Top margin on first page 10cm */
+            }
+            
+
+            /* *{
+                -webkit-print-color-adjust: exact!important;
+            } */
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row text-align">
+            {{-- <div class="col-md-3">
+                <div class="bgpeserta" style="width:302.3622047244088px; height:377.952755905511px; margin:0.5rem">
+                    <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+                        <div class="col-md-3 text-center">
+                            <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+                        </div>
+                        <div class="col-md-9 text-center" style="left:0.8rem">
+                            <p class="judul">PERMATA XXXIX</p>
+                            <p class="ket">CINTA ALAM INDONESIA</p>
+                            <p class="thn">2018</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12 text-center" style="left:1rem; top:1rem">
+                        <div class="kotak text-center">
+                            <label class="status">Peserta</label>
+                        </div>
+                    </div>
+                    <div class="col-md-12 text-center" style="top:2rem">
+                        <label class="inp">Khoirulli Nurul Fatimah</label>
+                        <label class="inp-kel">Sambutan</label>
+                        <label class="inp-bar">165634689567</label>
+                    </div>
+                </div>
+            </div> --}}
+            @foreach($get as $key => $value)
+            <div class="{{ $value->ket == 'panitia' ? 'bgpanitia' : ($value->ket == 'peserta' ? 'bgpeserta' : '') }}" style="width:13cm; height:17cm; margin-right:2rem;margin-bottom:8rem">
+                <div class="col-md-12 row" style="top:1rem; left:0.7rem">
+                    <div class="col-md-3 text-center">
+                        <img class="img" src="{{ asset('frontend/images/cai.png') }}" alt="" width="220%">
+                    </div>
+                    <div class="col-md-9 text-center" style="left:2.8rem; margin-top:0.5rem">
+                        <p class="judul">PERMATA XXXIX</p>
+                        <p class="ket">{{ $value->nama_kegiatan }}</p>
+                        <p class="thn">{{ year($value->tanggal_kegiatan) }}</p>
+                    </div>
+                </div>
+                <div class="col-md-12 text-center" style="left:5.6rem; top:1.9rem">
+                    <div class="kotak text-center">
+                        <label class="status">{{ $value->ket == 'panitia' ? 'Panitia' : ($value->ket == 'peserta' ? 'Peserta' : '') }}</label>
+                    </div>
+                </div>
+                <br>
+                <div class="col-md-12 text-center" style="top:2rem">
+                    <label class="inp   ">{{ $value->nama_anggota }}</label>
+                    <label class="inp-kel">{{ $value->nama_kelompok }}</label>
+                    {{--<label class="inp-bar"><img src="data:image/png;base64,{{$barcode->barcode($value->code_barcode)}}" alt="barcode"/>{{ $value->code_barcode }}</label>--}}
+                    <label class="inp-bar"><img src="data:image/png;base64,{{$barcode->barcode($value->code_barcode)}}"> {{--{{ $value->code_barcode }} --}}</label>
+                </div>
+            </div>
+            @if($key+1%4 == 0)
+            <div class="setpaper"></div>
+            @endif
+            @endforeach
+        </div>
+    </div>
+</body>
+</html>
